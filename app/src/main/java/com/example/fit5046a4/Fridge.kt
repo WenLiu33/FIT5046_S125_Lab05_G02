7a19d3b1459e3734e79c7500b7794f3613c12b88
Kelly <ktan0182@student.monash.edu>
2025-05-13 16:51:04 +1000

    //checking out new branch for implementation // previous branch was the UI for the fridge page, this has been merged to main without errors.

--- app/src/main/java/com/example/fit5046a4/Fridge.kt
+++ app/src/main/java/com/example/fit5046a4/Fridge.kt
@@ -42,7 +42,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
 @Composable
 fun Fridge() {
     val context = LocalContext.current
