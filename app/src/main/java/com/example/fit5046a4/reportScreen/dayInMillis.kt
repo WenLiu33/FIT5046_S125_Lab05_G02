e6ef5e63bd82635ad35ca96da7ace7f8af1c695b
WenLiu33 <tliu0075@student.monash.edu>
2025-05-21 00:28:02 +1000

    - add inline command in Report, BarChart, PieChart,

--- app/src/main/java/com/example/fit5046a4/reportScreen/dayInMillis.kt
+++ app/src/main/java/com/example/fit5046a4/reportScreen/dayInMillis.kt
@@ -1,4 +1,4 @@
-package com.example.fit5046a4.reportScreen   // ← put your actual package here
+package com.example.fit5046a4.reportScreen
 
 val Int.dayInMillis: Long
     get() = this * 24L * 60L * 60L * 1000L
