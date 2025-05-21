597604747bdfa2ae7e47f3408fb853e47b49568d
Lily Lan <mlan@deakin.edu.au>
2025-05-21 23:22:14 +1000

    Implemented logout function

--- app/src/main/java/com/example/fit5046a4/AppRoot.kt
+++ app/src/main/java/com/example/fit5046a4/AppRoot.kt
@@ -3,6 +3,10 @@ package com.example.fit5046a4
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -15,25 +19,39 @@ import com.google.firebase.auth.auth
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AppRoot() {
+    var isLoggedIn by remember { mutableStateOf(Firebase.auth.currentUser != null) }
+
     val navController = rememberNavController()
+    val startDestination = if (isLoggedIn) "MainApp" else "LoginDestination"
 
-    val isLoggedIn = Firebase.auth.currentUser!=null
-    val firstPage = if(isLoggedIn) "MainApp" else "LoginDestination"
-
-    NavHost(
-        navController = navController,
-        startDestination = firstPage
-    ) {
+    NavHost(navController = navController, startDestination = startDestination) {
         loginScreen(
-            onNavigateToMain = { navController.navigate("MainApp") },
-            onNavigateToRegister = {navController.navigateToRegister()}
+            onNavigateToMain = {
+                isLoggedIn = true
+                navController.navigate("MainApp") {
+                    popUpTo(0) { inclusive = true }
+                }
+            },
+            onNavigateToRegister = {
+                navController.navigateToRegister()
+            }
         )
         registerScreen(
-            onNavigateToMain = { navController.navigate("MainApp") },
-            onNavigateToLogin = {navController.navigateUp()}
+            onNavigateToMain = {
+                isLoggedIn = true
+                navController.navigate("MainApp") {
+                    popUpTo(0) { inclusive = true }
+                }
+            },
+            onNavigateToLogin = { navController.navigateUp() }
         )
         composable("MainApp") {
-            BottomNavigationBarAndTopBar()
+            BottomNavigationBarAndTopBar(
+                onLogout = {
+                    Firebase.auth.signOut()
+                    isLoggedIn = false
+                }
+            )
         }
     }
 }
